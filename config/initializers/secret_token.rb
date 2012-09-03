6d1577de5fe824205bbf742c13777fcc26404366
Kumquat <kumquat@kumquatexpress.(none)>
2012-09-03 14:11:23 -0400

    created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrippingOctoSansa::Application.config.secret_token = '4ad4b8043eff6ad8739c24ed8f3f1b94859f48e28f842a47e9b854fc923f322db5f84ef915abe67e0950cdc9bf4a06671d55645b6cd1fe934f31a6f71fd46314'
